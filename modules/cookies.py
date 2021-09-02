194631dc074b7a01cbfa6222cadf4f359b10d6b5
Jainam Oswal <me@jainam.me>
2021-09-02 12:31:12 +0530

    Few silly changes.

--- modules/cookies.py
+++ modules/cookies.py
@@ -1,10 +1,9 @@
 from app import app
 from flask import request, make_response
 
-
 @app.route('/cookies/set')
 def set():
-    res = make_response("Setting cookies...")
+    res = make_response({"message":"Cookies set!"})
 
     res.set_cookie("user", "JKumar", 100)
     res.set_cookie("country", "India", 100)
@@ -19,9 +18,9 @@ def get():
     return cookies
 
 
-@app.route('/cookies/clear')
+@app.route('/cookies/del')
 def clear():
-    res = make_response("Deleting cookies...")
+    res = make_response({"message":"Cookies deleted!"})
 
     res.delete_cookie("user")
     res.delete_cookie("country")
